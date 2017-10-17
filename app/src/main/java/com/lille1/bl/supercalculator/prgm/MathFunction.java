efa084a271333d853824dd927de79ab792337c3c
benji62f <benji62f@hotmail.fr>
2017-10-17 09:23:41 +0200

    Ajout de la documentation dans la class MathFunction

--- app/src/main/java/com/lille1/bl/supercalculator/prgm/MathFunction.java
+++ app/src/main/java/com/lille1/bl/supercalculator/prgm/MathFunction.java
@@ -1,6 +1,5 @@
 package com.lille1.bl.supercalculator.prgm;
 
-import android.app.Activity;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.os.AsyncTask;
@@ -8,8 +7,6 @@ import android.support.v4.app.NotificationCompat;
 import android.view.View;
 
 import com.lille1.bl.supercalculator.ui.MainActivity;
-
-import java.util.Calendar;
 
 /**
  * Created by Benjamin on 16/10/2017.
@@ -19,12 +16,7 @@ public class MathFunction {
     private String name;
     private int nbParams;
 
-    /**
-     * These attributes are used for the notification.
-     */
     private MainActivity activity;
-    private NotificationCompat.Builder mBuilder;
-    private NotificationManager mNotifyManager;
 
     public MathFunction(String name, int nbParams, MainActivity activity){
         this.name = name;
@@ -32,17 +24,22 @@ public class MathFunction {
         this.activity = activity;
     }
 
-    public double execute(double[] params){
+    public void execute(double[] params){
+        /**
+         * AsyncTask
+         */
         new ComplexeCalculation().execute(params);
 
-        return 1.0;
-        //return makeAVeryComplexeCalculation();
+        /**
+         * Sequential code
+         */
+        //makeAVeryComplexeCalculation(params);
     }
 
     /**
      * Simulates a long calculation
      */
-    private double makeAVeryComplexeCalculation(){
+    private void makeAVeryComplexeCalculation(double[] params){
         try {
             for(int i=1 ; i<=10 ; i++) { // waits 10 seconds
                 synchronized (this) {
@@ -53,29 +50,45 @@ public class MathFunction {
             e.printStackTrace();
         }
 
-        return Math.random()*100+1;
+        double result = Math.random()*100+1;
+        activity.getResults().add(new Result(getName(), params, result)); // records the result in the result list
     }
 
     private class ComplexeCalculation extends AsyncTask<double[], Integer, Double> {
         private double[] params;
         private int id;
 
+        /**
+         * These attributes are used for the notification.
+         */
+        private NotificationCompat.Builder mBuilder;
+        private NotificationManager mNotifyManager;
+
+        /**
+         * This is the main method of the AsyncTask. This is used to simulate a hard calculation.
+         * @param params Calculation params
+         * @return Calculation result
+         */
         protected Double doInBackground(double[]... params) {
-            this.params = params[0];
-            try {
+            this.params = params[0]; // retrieves params
+            try { // waits 10 seconds
                 for(int i=1 ; i<=10 ; i++) {
                     synchronized (this) {
                         wait(1000);
                     }
-                    publishProgress(i*10);
+                    publishProgress(i*10); // calls onProgressUpdate
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
 
-            return Math.random()*100+1;
+            return Math.random()*100+1; // returns calculation result
         }
 
+        /**
+         * This method is called before the AsyncTask doInBackground method.
+         * This is used to create the notification progress bar.
+         */
         protected void onPreExecute(){
             mNotifyManager =
                     (NotificationManager) activity.getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
@@ -88,19 +101,27 @@ public class MathFunction {
             mNotifyManager.notify(id, mBuilder.build());
         }
 
+        /**
+         * This method is called during the AsyncTask doInBackground method.
+         * This is used to update the notification progress value.
+         */
         protected void onProgressUpdate(Integer... progress) {
             mBuilder.setProgress(100, progress[0], false)
                     .setContentText(name + ": " + progress[0] + " %");
             mNotifyManager.notify(id, mBuilder.build());
         }
 
+        /**
+         * This method is called after the AsyncTask doInBackground method.
+         * This is used to remove the notification progress bar.
+         */
         protected void onPostExecute(Double result) {
             mBuilder.setContentText("Calculation complete")
                     // Removes the progress bar
                     .setProgress(0, 0, false);
             mNotifyManager.notify(id, mBuilder.build());
 
-            activity.getResults().add(new Result(getName(), this.params, result));
+            activity.getResults().add(new Result(getName(), this.params, result)); // records the result in the result list
         }
     }
 
